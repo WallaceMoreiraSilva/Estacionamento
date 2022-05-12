dc05e7543ded7c857351ff2a7231eb3290a05467
wallace moreira da silva <wallaceinfofuturo@gmail.com>
2022-05-12 19:47:12 -0300

    Padrao AAA

--- Estacionamento/Estacionamento.Testes/VeiculoTestes.cs
+++ Estacionamento/Estacionamento.Testes/VeiculoTestes.cs
@@ -9,16 +9,22 @@ namespace Estacionamento.Testes
         [Fact]
         public void TestaVeiculoAcelerar()
         {
+            //Arrange
             var veiculo = new Veiculo();
+            //Act
             veiculo.Acelerar(10);
+            //Assert
             Assert.Equal(100, veiculo.VelocidadeAtual);
         }
 
         [Fact]
         public void TestaVeiculoFrear()
         {
+            //Arrange
             var veiculo = new Veiculo();
+            //Act
             veiculo.Frear(10);
+            //Assert
             Assert.Equal(-150, veiculo.VelocidadeAtual);
         }
     }
